1cdd49354b59c4e60c970c32e9904b490727f433
UlianaPlisko <ulianapl1@gmail.com>
2025-12-31 02:39:14 +0100

    fix arm64 version

--- elvira-dashboard/src/App.tsx
+++ elvira-dashboard/src/App.tsx
@@ -1,13 +1,11 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import UseCase1 from './components/UseCase1'; // Rename your original App.tsx to UseCase1.tsx
-import UseCase2 from './components/UseCase2'; // The new component above
 
 export default function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<UseCase1 />} /> {/* Original use case 1 */}
-        <Route path="/use-case2" element={<UseCase2 />} />
       </Routes>
     </Router>
   );
